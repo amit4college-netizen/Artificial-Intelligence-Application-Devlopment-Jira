6e55d561a16e1e698fcd4be8e80da6eb6cbc8872
amit4college-netizen <amit4college@gmail.com>
2026-08-04 05:24:23 +0000

    6

--- Practical-6.py
+++ Practical-6.py
@@ -3,4 +3,5 @@ import heapq
 #display puzzle state
 def print_state(state):
     for i in range (0,9,3):
-        print(state[i],state[i+1],)
+        print(state[i],state[i+1],state[i+2])
+    print()
